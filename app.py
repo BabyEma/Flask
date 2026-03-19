57c99021924e1885192c952a1c1c7ed045820d5b
BabyEma <emapsp22@icloud.com>
2026-03-18 22:13:56 -0400

    Primer commit: subiendo proyecto Flask

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/pagina1')
+def pagina1():
+    nombres = ["Juan", "Ana", "Luis"]
+    return render_template('pagina1.html', nombres=nombres)
+
+@app.route('/pagina2')
+def pagina2():
+    usuarios = [
+        {"nombre": "Carlos", "edad": 20},
+        {"nombre": "María", "edad": 25}
+    ]
+    return render_template('pagina2.html', usuarios=usuarios)
+
+if __name__ == '__main__':
+    app.run(debug=True)
